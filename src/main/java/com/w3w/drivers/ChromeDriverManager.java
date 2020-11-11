f631231311c37cff586abc04dbc73f736501f7e7
arkhipchuk.hanna <aranvic1976@gmail.com>
2020-11-11 22:12:08 +0200

    circleci simple config

--- src/main/java/com/w3w/drivers/ChromeDriverManager.java
+++ src/main/java/com/w3w/drivers/ChromeDriverManager.java
@@ -1,12 +1,14 @@
 package com.w3w.drivers;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeDriverManager extends DriverManager{
     public void createDriver(){
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
-        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
+//        System.setProperty("webdriver.chrome.driver", "./drivers/chromedriver.exe");
         options.addArguments("-lang=en");
         options.addArguments("start-maximized");
         this.webDriver = new ChromeDriver(options);
